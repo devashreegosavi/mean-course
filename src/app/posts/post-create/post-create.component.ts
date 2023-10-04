33a5fcfac17fefc633a806e9d09e7348c6b3f569
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-10-04 13:37:46 +0530

    4 oct 2023

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -55,7 +55,8 @@ export class PostCreateComponent implements OnInit{
                     this.post = {
                         id: postData._id, 
                         title : postData.title, 
-                        content : postData.content
+                        content : postData.content,
+                        imagePath : null
                     };
                     this.form.setValue({
                         'title' : this.post.title, 
@@ -78,13 +79,14 @@ export class PostCreateComponent implements OnInit{
         const post : Post = {
             id : this.form.value.id,
             title : this.form.value.title ,
-            content : this.form.value.content
+            content : this.form.value.content,
+            imagePath : this.form.value.image
         };
         if(this.mode === 'create'){
             this.postsService.addPost(this.form.value.id, this.form.value.title, this.form.value.content, this.form.value.image);
         }
         else{
-            this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content)
+            this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content, this.form.value.image)
         }
         //this.postCreated.emit(post);
         
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -22,7 +22,8 @@ export class PostsService {
           return {
             title : post.title,
             content : post.content,
-            id : post._id
+            id : post._id,
+            imagePath : post.imagePath
           };
         });
     }))
@@ -43,9 +44,14 @@ export class PostsService {
     postData.append("image",image, title);
     //const post : Post = {id : null, title : title, content : content};
     //this.http.post<{message : string, postId : string }>('http://localhost:3000/api/posts',post)
-    this.http.post<{message : string, postId : string }>('http://localhost:3000/api/posts',postData)
+    this.http.post<{message : string, post : Post }>('http://localhost:3000/api/posts',postData)
     .subscribe((responseData) => {
-      const post : Post = {id : responseData.postId, title : title, content : content};
+      const post : Post = {
+        id : responseData.post.id,
+        title : title,
+        content : content,
+        imagePath : responseData.post.imagePath
+      };
       //console.log(responseData.message);
       //const id = responseData.postId;
       //post.id=id;
@@ -56,11 +62,12 @@ export class PostsService {
     
   }
 
-  updatePost(id: string, title : string, content : string){
+  updatePost(id: string, title : string, content : string, imagePath : null){
     const post : Post = { 
       id: id,
       title : title,
-      content : content
+      content : content,
+      imagePath : null
     };
     this.http.put("http://localhost:3000/api/posts/"+ id, post)
     .subscribe(response => {
