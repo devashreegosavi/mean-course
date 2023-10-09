c366950f4c30142081aef0468f0ec471ec68b0dc
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-10-09 18:02:54 +0530

    9 oct 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from './posts/post.model';
+import { AuthService } from './auth/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mean-course';
 
   /*storedPosts : Post[] = [];
@@ -13,4 +14,10 @@ export class AppComponent {
   onPostAdded(post){
     this.storedPosts.push(post);
   }*/
+  constructor(private authService : AuthService){
+
+  }
+  ngOnInit(): void {
+      this.authService.autoAuthUser();
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -43,9 +43,7 @@ export class AuthService{
                 this.token = token;
                 if(token){
                     const expiresInDuration = response.expiresIn;
-                    this.tokenTimer = setTimeout(() => {
-                        this.logout();
-                    }, expiresInDuration * 1000);
+                    this.setAuthTimer(expiresInDuration);
                     this.isAuthenticated = true;
                     this.authStatusListener.next(true);
                     const now = new Date();
@@ -59,6 +57,24 @@ export class AuthService{
 
     autoAuthUser(){
         const authInformation = this.getAuthData();
+        if(!authInformation){
+            return;
+        }
+        const now = new Date();
+        const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+        if(expiresIn > 0 ){
+            this.token = authInformation.token;
+            this.isAuthenticated =true;
+            this.setAuthTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+        }   
+    }
+
+    private setAuthTimer(duration:number){
+        console.log("Setting Timer "+ duration);
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, duration * 1000);
     }
 
     logout(){
@@ -84,8 +100,8 @@ export class AuthService{
     private getAuthData(){
         const token =localStorage.getItem("token");
         const expirationDate = localStorage.getItem("expiration");
-        if(!token && !expirationDate){
-            return;
+        if(!token || !expirationDate){
+            //return;
         }
         return {
             token : token,
